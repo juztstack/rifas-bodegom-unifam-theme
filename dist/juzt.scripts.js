a011ea20d33b60a9ac600e560550b78c9662bffe
Jesus Uzcategui <uzcateguijesusdev@gmail.com>
2025-10-22 09:59:16 -0500

    build assets

--- dist/juzt.scripts.js
+++ dist/juzt.scripts.js
@@ -1,26 +1,2 @@
-/******/ (() => { // webpackBootstrap
-/*!*************************!*\
-  !*** ./src/js/index.js ***!
-  \*************************/
-document.addEventListener("DOMContentLoaded", function () {
-  // Toggle menú móvil
-  const mobileMenuButton = document.getElementById('mobile-menu-button');
-  const mobileMenu = document.getElementById('mobile-menu');
-  mobileMenuButton && mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-  });
-
-  // Toggle submenús móviles
-  const mobileSubmenuToggles = document.querySelectorAll('.mobile-submenu-toggle');
-  mobileSubmenuToggles.forEach(toggle => {
-    toggle.addEventListener('click', () => {
-      const submenu = toggle.nextElementSibling;
-      const icon = toggle.querySelector('svg');
-      submenu.classList.toggle('active');
-      icon.classList.toggle('rotate-180');
-    });
-  });
-});
-/******/ })()
-;
+document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("mobile-menu-button"),t=document.getElementById("mobile-menu");e&&e.addEventListener("click",()=>{t.classList.toggle("active")}),document.querySelectorAll(".mobile-submenu-toggle").forEach(e=>{e.addEventListener("click",()=>{const t=e.nextElementSibling,n=e.querySelector("svg");t.classList.toggle("active"),n.classList.toggle("rotate-180")})})});
 //# sourceMappingURL=juzt.scripts.js.map
